dd20effeac2f151530600c429d5ba4eee2260d69
obwansan <kob123@hotmail.co.uk>
2018-04-30 15:17:45 +0100

    created PDO to select products from DB

--- src/classes/product.php
+++ src/classes/product.php
@@ -2,6 +2,17 @@
 
 namespace store;
 
+
+$db = new PDO('mysql:host=127.0.0.1; dbname=ecommerceStore2018', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare("SELECT * FROM products WHERE categoryId = 1");
+$query->execute();
+$result = $query->fetchAll(PDO::FETCH_CLASS, 'Product');
+
+
 class Product {
 
 }
+
+var_dump($result);
