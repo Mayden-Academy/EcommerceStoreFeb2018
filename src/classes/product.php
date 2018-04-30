4d186937120e1a618e808fd3030feaa3a21b9464
obwansan <kob123@hotmail.co.uk>
2018-04-30 16:06:24 +0100

    created PDO and SQL select statement to get products from DB

--- src/classes/product.php
+++ src/classes/product.php
@@ -1,18 +1,29 @@
 <?php
 
-namespace store;
 
+
+namespace store;
+use \PDO;
 
 $db = new PDO('mysql:host=127.0.0.1; dbname=ecommerceStore2018', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$query = $db->prepare("SELECT * FROM products WHERE categoryId = ':categoryId'");
 
-$query = $db->prepare("SELECT * FROM products WHERE categoryId = 1");
+$categoryId = 1;
+var_dump($_GET['id']);
+
+$query->bindParam(':categoryId', $categoryId);
 $query->execute();
-$result = $query->fetchAll(PDO::FETCH_CLASS, 'Product');
-
+$result = $query->fetchAll(PDO::FETCH_CLASS, 'store\Product');
 
 class Product {
-
+    private $id;
+    private $categoryId;
+    private $productName;
+    private $productPrice;
+    private $productDescription;
+    private $availableSizes;
+    private $availableColors;
+    private $deleted;
 }
 
-var_dump($result);
