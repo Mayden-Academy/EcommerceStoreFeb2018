a154a4f829ee11e621c90f631f5bd8f984f33b7b
JamesCullen2 <jamesnac@aol.com>
2018-04-30 15:15:02 +0100

    fixed botstrap linking and meta tags

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Totally Pawesome</title>
-    <link href="src/assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="src/assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="src/assets/css/styles.css">
 </head>
 <body>
 <aside class="navbar-left">  </aside>
