4093a80136b3e52db6c282294e26684a63e25980
ailindoyle <caitlin.doyle@mobile-life.com>
2018-05-01 16:24:07 +0100

    uppercase variable changed to lowercase

--- src/classes/Store.php
+++ src/classes/Store.php
@@ -7,7 +7,7 @@ use Store\interfaces\GetCategories as GetCategories;
 
 class Store implements GetCategories{
 
-    static private $DB;
+    static private $db;
 
     /**
      *Database query to get category data
@@ -15,12 +15,12 @@ class Store implements GetCategories{
      * @return array array of objects of category class
      */
     public static function getCategories():array {
-        $query = self::$DB->prepare("SELECT `id`, `categoryName`, `defaultImageFilePath`,`defaultImageAlt` FROM `categories` WHERE `deleted` = 0;");
+        $query = self::$db->prepare("SELECT `id`, `categoryName`, `defaultImageFilePath`,`defaultImageAlt` FROM `categories` WHERE `deleted` = 0;");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_CLASS, Category::class);
     }
 
     public static function setPDO(PDO $db){
-        self::$DB = $db;
+        self::$db = $db;
     }
 }
