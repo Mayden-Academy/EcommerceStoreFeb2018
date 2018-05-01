4d84e8812054834bdb0cef70c0e14b29343a3539
Karim Kefi <karimkefi@gmail.com>
2018-05-01 10:34:46 +0100

    changed DBConnect into its own class and own file

---
+++ src/classes/DBConnect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace store;
+require_once "../../vendor/autoload.php";
+
+use \PDO;
+use \Exception;
+
+class DBConnect {
+    public static function connectToDB() {
+        try{
+            return new PDO('mysql:host=127.0.0.1; dbname=ecommerce-shop', 'root');
+        } catch (Exception $e) {
+            echo "your connection did not work: " . $e->getMessage();
+        }
+    }
+}
--- src/classes/Store.php
+++ src/classes/Store.php
@@ -2,47 +2,31 @@
 
 namespace Store;
 use Store\Category as Category;
-use \PDO;
-use \Exception;
-
+use Store\DBConnect as DBConnect;
 require_once "../../vendor/autoload.php";
-
-interface dbConnectable {
-    public function connectToDB();
-}
-
-
-
-trait dBConnect {
-    public $db;
-
-    public function connectToDB() {
-        try{
-            $this->db = new PDO('mysql:host=127.0.0.1; dbname=ecommerce-shop', 'root');
-        } catch (Exception $e) {
-            echo "your connection did not work: " . $e->getMessage();
-        }
-    }
-
-}
-
+use \PDO;
 
 
 class Store {
-    use dBConnect;
+
+    private $dBConnection;
 
     public function getCategories(){
-        $query = $this->db->prepare("SELECT `id`, `categoryName` FROM `categories` WHERE `deleted` = 0;");
+        $query = $this->dBConnection->prepare("SELECT `id`, `categoryName` FROM `categories` WHERE `deleted` = 0;");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_CLASS, Category::class);
     }
 
-    public function __construct() {
-        $this->connectToDB();
+    public function __construct($db) {
+        $this->dBConnection = $db;
     }
+
+
 
 }
 
 
-$bob = new Store();
+$i = DBConnect::connectToDB();
+
+$bob = new Store($i);
 echo '<pre>' . var_export($bob->getCategories(), true) . '</pre>';
