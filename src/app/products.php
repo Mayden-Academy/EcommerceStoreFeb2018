bc15277eb2d6fc3455e411837fdfd69f4a3da801
ailindoyle <caitlin.doyle@mobile-life.com>
2018-05-01 11:59:03 +0100

    structure template for products, will contain all products of one category

--- src/app/products.php
+++ src/app/products.php
@@ -1,1 +1,82 @@
-<?php
+<?php //require_once __DIR__ . '../../vendor/autoload.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Totally Pawesome</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+</head>
+<body>
+<section class="row banner">
+    <div class="home col-xs-2">
+        <a href="#">
+            <h2>Home</h2>
+        </a>
+    </div>
+    <div class="shop-name col-xs-10">
+        <h2>Totally Pawesome</h2>
+    </div>
+</section>
+<section class="row main">
+    <div class="sidebar col-xs-2">
+        <a href="#">
+            <h4>Category1</h4>
+        </a>
+        <a href="#">
+            <h4>Category2</h4>
+        </a>
+        <a href="#">
+            <h4>Category3</h4>
+        </a>
+        <a href="#">
+            <h4>Category4</h4>
+        </a>
+        <a href="#">
+            <h4>Category5</h4>
+        </a>
+        <a href="#">
+            <h4>Category6</h4>
+        </a>
+    </div>
+    <div class="col-xs-10">
+        <div class="row">
+            <h2>Product Category</h2>
+        </div>
+        <div class="row">
+            <div class="product col-xs-8 col-sm-4 col-md-3">
+                <a href="#">
+                    <img class="img-thumbnail" src="../assets/img/cat2.jpg">
+                    <h4>Product Name</h4>
+                    <h5>£00.00</h5>
+                </a>
+            </div>
+            <div class="product col-xs-8 col-sm-4 col-md-3">
+                <a href="#">
+                    <img class="img-thumbnail" src="../assets/img/cat2.jpg">
+                    <h4>Product Name</h4>
+                    <h5>£00.00</h5>
+                </a>
+            </div>
+            <div class="product col-xs-8 col-sm-4 col-md-3">
+                <a href="#">
+                    <img class="img-thumbnail" src="../assets/img/cat2.jpg">
+                    <h4>Product Name</h4>
+                    <h5>£00.00</h5>
+                </a>
+            </div>
+            <div class="product col-xs-8 col-sm-4 col-md-3">
+                <a href="#">
+                    <img class="img-thumbnail" src="../assets/img/cat2.jpg">
+                    <h4>Product Name</h4>
+                    <h5>£00.00</h5>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
