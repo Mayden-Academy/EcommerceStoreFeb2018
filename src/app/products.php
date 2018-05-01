2afcc8fb97b97c0d13ca17a8a3343e754672983d
obwansan <kob123@hotmail.co.uk>
2018-05-01 09:10:14 +0100

    latest update of products and product

--- src/app/products.php
+++ src/app/products.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace store;
+
+use \PDO;
+use \store\Product;
+
+$db = new PDO('mysql:host=127.0.0.1; dbname=ecommerceStore2018', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare("SELECT * FROM products WHERE categoryId = :categoryId");
+
+$categoryId = '1';
+//var_dump($_GET['id']);
+
+$query->bindParam(':categoryId', $categoryId);
+$query->execute();
+$result = $query->fetchAll(PDO::FETCH_CLASS, 'store/Product');
+
+var_dump($result);
--- src/classes/product.php
+++ src/classes/product.php
@@ -1,22 +1,24 @@
 <?php
 
-
-
 namespace store;
 use \PDO;
 
 $db = new PDO('mysql:host=127.0.0.1; dbname=ecommerceStore2018', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare("SELECT * FROM products WHERE categoryId = ':categoryId'");
 
-$categoryId = 1;
-var_dump($_GET['id']);
+$query = $db->prepare("SELECT * FROM products WHERE categoryId = :categoryId");
+
+$categoryId = '1';
+//var_dump($_GET['id']);
 
 $query->bindParam(':categoryId', $categoryId);
 $query->execute();
 $result = $query->fetchAll(PDO::FETCH_CLASS, 'store\Product');
 
-class Product {
+var_dump($result);
+
+class Product
+{
     private $id;
     private $categoryId;
     private $productName;
@@ -25,5 +27,40 @@ class Product {
     private $availableSizes;
     private $availableColors;
     private $deleted;
-}
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    public function getProductName()
+    {
+        return $this->productName;
+    }
+
+    public function getProductPrice()
+    {
+        return $this->productPrice;
+    }
+
+    public function getProductDescription()
+    {
+        return $this->productDescription;
+    }
+
+    public function getAvailableSizes()
+    {
+        return $this->availableSizes;
+    }
+
+    public function getAvailableColors()
+    {
+        return $this->availableColors;
+    }
+
+}
