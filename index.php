682bf5b203de541767b132ec7c56cce594c8b4db
ailindoyle <caitlin.doyle@mobile-life.com>
2018-05-01 13:51:28 +0100

    index.html refactored to remove container non-responsiveness

--- index.php
+++ index.php
@@ -10,16 +10,20 @@
     <link rel="stylesheet" type="text/css" href="src/assets/css/vendor/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="src/assets/css/styles.css">
 </head>
+
 <body>
-    <div class="row">
-        <nav class="navbar-fixed-top col-xs-9 col-sm-10 col-xs-offset-3 col-sm-offset-2">
-            <h1>Totally Pawesome</h1>
-        </nav>
-    </div>
-    <aside class="col-xs-3 col-sm-2">
+<section class="row banner">
+    <div class="home col-xs-2">
         <a href="#">
             <h2>Home</h2>
         </a>
+    </div>
+    <div class="shop-name col-xs-10">
+        <h2>Totally Pawesome</h2>
+    </div>
+</section>
+<section class="row main">
+    <div class="sidebar col-xs-2">
         <a href="#">
             <h4>Category1</h4>
         </a>
@@ -38,47 +42,51 @@
         <a href="#">
             <h4>Category6</h4>
         </a>
-    </aside>
-    <main class="container col-xs-offset-3 col-sm-offset-2">
+    </div>
+    <div class="main-content col-xs-10">
         <div class="row">
-            <h1>Categories</h1>
-            <div class="col-xs-8 col-sm-4">
+            <h2>Categories</h2>
+        </div>
+        <div class="row index-tiles">
+            <div class="category col-xs-10 col-sm-6 col-md-4">
                 <a href="#">
                     <img class="img-thumbnail" src="src/assets/img/cat1.jpg">
-                    <h4>Category1</h4>
+                    <h4>Category 1</h4>
                 </a>
             </div>
-            <div class="col-xs-8 col-sm-4">
+            <div class="category col-xs-10 col-sm-6 col-md-4">
                 <a href="#">
                     <img class="img-thumbnail" src="src/assets/img/cat1.jpg">
-                    <h4>Category2</h4>
+                    <h4>Category 2</h4>
                 </a>
             </div>
-            <div class="col-xs-8 col-sm-4">
+            <div class="category col-xs-10 col-sm-6 col-md-4">
                 <a href="#">
                     <img class="img-thumbnail" src="src/assets/img/cat1.jpg">
-                    <h4>Category3</h4>
+                    <h4>Category 3</h4>
                 </a>
             </div>
-            <div class="col-xs-8 col-sm-4">
+            <div class="category col-xs-10 col-sm-6 col-md-4">
                 <a href="#">
                     <img class="img-thumbnail" src="src/assets/img/cat1.jpg">
-                    <h4>Category4</h4>
+                    <h4>Category 4</h4>
                 </a>
             </div>
-            <div class="col-xs-8 col-sm-4">
+            <div class="category col-xs-10 col-sm-6 col-md-4">
                 <a href="#">
                     <img class="img-thumbnail" src="src/assets/img/cat1.jpg">
-                    <h4>Category5</h4>
+                    <h4>Category 5</h4>
                 </a>
             </div>
-            <div class="col-xs-8 col-sm-4">
+            <div class="category col-xs-10 col-sm-6 col-md-4">
                 <a href="#">
                     <img class="img-thumbnail" src="src/assets/img/cat1.jpg">
-                    <h4>Category6</h4>
+                    <h4>Category 6</h4>
                 </a>
             </div>
         </div>
-    </main>
+    </div>
+</section>
 </body>
+
 </html>
