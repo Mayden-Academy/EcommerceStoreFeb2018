bf84fcc6602ab02f0ee1c4d06e8075c5d2160d23
Nick Santiago Blair <nsantiagoblair@gmail.com>
2018-05-01 15:13:40 +0100

    add test for Product

--- src/test/classes/Product.php
+++ src/test/classes/Product.php
@@ -1,2 +1,21 @@
 <?php
 
+require_once '../../classes/Product.php';
+use PHPUnit\Framework\TestCase;
+
+//use store\Product;
+
+class ProductTest extends TestCase
+{
+
+    public function testproductIsObject()
+    {
+        $case = new \Store\Product();
+        $expected = true;
+        $this->assertEquals(is_object($case), $expected);
+    }
+//  Not testing Product methods because are only getters. Would need to use ReflectionClass to make
+// private properties public and set them manually. See: http://www.webtipblog.com/unit-testing-private-methods-and-properties-with-phpunit/
+}
+
+
