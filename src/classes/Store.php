c6ee4bc6f2772354f798e19d78c8c268eb53935c
C0deJack <jackwoodpearce@gmail.com>
2018-05-02 10:41:17 +0100

    In the process of refactoring the code to remove static classes but commiting midway to merge story2 thats in master now up to story4

--- src/classes/Store.php
+++ src/classes/Store.php
@@ -7,30 +7,30 @@ use Store\interfaces\GetCategories as GetCategories;
 
 class Store implements GetCategories{
 
-    static private $DB;
+    private $db;
 
     /**
      *Database query to get category data
      *
      * @return array array of objects of category class
      */
-    public static function getCategories():array {
-        $query = self::$DB->prepare("SELECT `id`, `categoryName`, `defaultImageFilePath`,`defaultImageAlt` FROM `categories` WHERE `deleted` = 0;");
+    public function getCategories():array {
+        $query = $this->db->prepare("SELECT `id`, `categoryName`, `defaultImageFilePath`,`defaultImageAlt` FROM `categories` WHERE `deleted` = 0;");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_CLASS, Category::class);
     }
 
-    public static function setPDO(PDO $db){
-        self::$DB = $db;
-    }
+//    public function setPDO(PDO $db){
+//        $this->DB = $db;
+//    }
 
     /**
      *Database query to get product data
      *
      * @return array of objects of product class
      */
-    public static function getProductPage($id):array {
-        $query2 = self::$DB->prepare("SELECT `products`.`id`, 
+    public function getProductPage($id):array {
+        $query2 = $this->db->prepare("SELECT `products`.`id`, 
         `products`.`productName`, `products`.`productPrice`, `products`.`availableColors`
         ,`products`.`availableSizes`, `products`.`productDescription`,`images`.`imageRef`
           FROM `products` LEFT JOIN `images` ON `products`.`id` = `images`.`productId`
