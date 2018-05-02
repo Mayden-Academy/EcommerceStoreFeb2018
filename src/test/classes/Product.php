e9eddbdf7c87678895a90e8c4fafa6044e8fe025
Nick Santiago Blair <nsantiagoblair@gmail.com>
2018-05-02 09:14:00 +0100

    correct grammar in comment

--- src/test/classes/Product.php
+++ src/test/classes/Product.php
@@ -14,7 +14,7 @@ class ProductTest extends TestCase
         $expected = true;
         $this->assertEquals(is_object($case), $expected);
     }
-//  Not testing Product methods because are only getters. Would need to use ReflectionClass to make
+//  Not testing Product methods because they are only getters. Would need to use ReflectionClass to make
 // private properties public and set them manually. See: http://www.webtipblog.com/unit-testing-private-methods-and-properties-with-phpunit/
 }
 
